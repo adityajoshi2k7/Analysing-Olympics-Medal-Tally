de2a9ffbbdc5b7afc7a4ce396942f16005b46551
Arpita Shekhar <ashekha@ncsu.edu>
2018-11-04 13:50:47 -0500

    first attempt

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-test
+#!/usr/bin/python
+import numpy
+import pandas
+
+df = pandas.read_csv('athlete_events.csv')
+
+# remove winter entries
+df = df[~df.Season.str.contains("Winter")]
+#print(df)
+
+sport_year =  df.groupby(['Sport', 'Year']).Year.count()
+
+print(sport_year)
+#['Year']['count'].sort_values() 			#.agg({'Year': ['count']})
+#print(df.groupby(['Year', 'Team', 'Event', 'Medal']).agg('sum').reset_index())
+#print(df.groupby(['Sport','Year']).count())
