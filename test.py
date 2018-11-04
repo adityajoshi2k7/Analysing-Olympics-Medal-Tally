8a70a5269e27e17a7db28dd3bc014d217d717889
Arpita Shekhar <ashekha@ncsu.edu>
2018-11-04 15:03:41 -0500

    Final Data

--- test.py
+++ test.py
@@ -2,15 +2,24 @@
 import numpy
 import pandas
 
-df = pandas.read_csv('athlete_events.csv')
+original = pandas.read_csv('athlete_events.csv')
 
 # remove winter entries
-df = df[~df.Season.str.contains("Winter")]
+summer = original[~original.Season.str.contains("Winter")]
 #print(df)
+latest_games = summer['Year'] > 2004
+recent = summer[latest_games]
 
-sport_year =  df.groupby(['Sport', 'Year']).Year.count()
+# find recent sports; remove sports not played now
+recent_sports =  recent[['Sport', 'Year']].drop_duplicates().groupby('Sport')['Year'].count().reset_index()['Sport']
+recent_sports = summer.loc[summer['Sport'].isin(recent_sports)]
 
-print(sport_year)
-#['Year']['count'].sort_values() 			#.agg({'Year': ['count']})
-#print(df.groupby(['Year', 'Team', 'Event', 'Medal']).agg('sum').reset_index())
-#print(df.groupby(['Sport','Year']).count())
+# remove sports with insufficient data
+recent_sports = recent_sports[['Sport', 'Year']].drop_duplicates().groupby("Sport")['Year'].count().reset_index()
+recent_sports = recent_sports[recent_sports.Year > 6]['Sport']
+
+final_data = summer.loc[summer['Sport'].isin(recent_sports)]
+print(final_data.loc[final_data.Sport == 'Badminton'])
+
+
+#
