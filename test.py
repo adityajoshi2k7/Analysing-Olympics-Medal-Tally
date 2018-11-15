91ef2416f6d5c052a085befe64bf6e088aeb0407
Arpita Shekhar <ashekha@ncsu.edu>
2018-11-15 14:11:34 -0500

    final decision tree

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ original = pandas.read_csv('athlete_events.csv')
 
 # remove winter entries
 summer = original[~original.Season.str.contains("Winter")]
-summer = summer.drop(columns = ['Season', 'Games', 'Name', 'ID', 'Event', 'City'])
+summer = summer.drop(columns = ['Season', 'Games', 'Name', 'ID', 'Event', 'City', 'NOC'])
 latest_games = summer['Year'] > 2004
 recent = summer[latest_games]
 
@@ -95,14 +95,12 @@ y_test = y_test.replace(np.nan, 'No', regex = True)
 
 # Encode string data for Decision Tree classifier
 X_train['Sex'],_ = pandas.factorize(X_train['Sex'])
-X_train['NOC'],_ = pandas.factorize(X_train['NOC'])
 X_train['Sport'],_ = pandas.factorize(X_train['Sport'])
 X_train['Team'],_ = pandas.factorize(X_train['Team'])	
 
 y_train['Medal'],_ = pandas.factorize(y_train['Medal'])
 
 X_test['Sex'],_ = pandas.factorize(X_test['Sex'])
-X_test['NOC'],_ = pandas.factorize(X_test['NOC'])
 X_test['Sport'],_ = pandas.factorize(X_test['Sport'])
 X_test['Team'],_ = pandas.factorize(X_test['Team'])	
 
@@ -110,24 +108,27 @@ y_test['Medal'],_ = pandas.factorize(y_test['Medal'])
 
 # Gini Classifier
 def decision_tree(classifier):
-    dec_classifier = DecisionTreeClassifier(criterion = classifier, random_state = 100, max_depth = 1, random_state = 10)
+    dec_classifier = DecisionTreeClassifier(criterion = classifier, random_state = 100, max_depth = 4)
     dec_classifier.fit(X_train, y_train)
 
     features = list(X_train.head(0))
+
     if classifier=='gini':
-		export_graphviz(dec_classifier, out_file = 'gini_tree.dot')
+        export_graphviz(dec_classifier, out_file = 'gini_tree.dot', feature_names = X_train.columns)
+        check_call(['dot','-Tpng','gini_tree.dot','-o','gini_OutputFile.png'])
+    else :
+        export_graphviz(dec_classifier, out_file = 'entropy_tree.dot', feature_names = X_train.columns)
+        check_call(['dot','-Tpng','entropy_tree.dot','-o','entropy_OutputFile.png'])
 
-		check_call(['dot','-Tpng','gini_tree.dot','-o','gini_OutputFile.png'])
-	else :
-		export_graphviz(dec_classifier, out_file = 'entropy_tree.dot')
-
-		check_call(['dot','-Tpng','entropy_tree.dot','-o','entropy_OutputFile.png'])
     y_pred = dec_classifier.predict(X_test)
     print('Classifier :', classifier)
     print('\nAccuracy: ', accuracy_score(y_test, y_pred) * 100)
     precision, recall, fscore, support = precision_recall_fscore_support(y_test, y_pred, average = 'micro')
-    print('\nPrecision: %f Recall: %f F-score: %f', precision, recall, fscore)
+    print('\nPrecision: ', precision, '\nRecall: ', recall, '\nF-score: ', fscore)
 
 decision_tree('gini')
 decision_tree('entropy')
+
+
+
 
