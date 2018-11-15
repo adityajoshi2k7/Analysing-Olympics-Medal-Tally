0090c5fa61b972120b2f3d51a4d9c34f5d480f3e
Arpita Shekhar <ashekha@ncsu.edu>
2018-11-15 12:30:58 -0500

    Graphical representations added

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas
 from subprocess import check_call
+import seaborn as sns
+from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
@@ -24,10 +26,59 @@ recent_sports = recent_sports[recent_sports.Year > 6]['Sport']
 # keep sports found in recent sports
 final_data = summer.loc[summer['Sport'].isin(recent_sports)]
 
+# Null and NA values - count
+print('\nNull values per attribute: \n', final_data.isnull().sum())
+
 # replace NA values with column mean
 final_data['Height'].fillna((final_data['Height'].mean()), inplace = True)
 final_data['Weight'].fillna((final_data['Weight'].mean()), inplace = True)
 final_data['Age'].fillna((final_data['Age'].mean()), inplace = True)
+print('\nCorrelation b/w Age, Height and Weight: \n', final_data[['Age', 'Height', 'Weight']].corr())
+print('\n', final_data.describe())
+
+# Medal Tally - Top 10 countries
+medals_country = final_data.groupby(['Team','Medal'])['Sex'].count().reset_index().sort_values(by = 'Sex', ascending = False)
+medals_country = medals_country.pivot('Team', 'Medal', 'Sex').fillna(0)
+top = medals_country.sort_values(by = 'Gold', ascending = False)[:10]
+top.plot.barh(width = 0.8, color=['#e78ae0', '#7eaee5', '#49ae7f'])
+fig = plt.gcf()
+fig.set_size_inches(12,12)
+plt.title('Medals Distribution Of Top 10 Countries')
+
+# Distribution of Gold Medals vs Age
+gold_medals = final_data[(final_data.Medal == 'Gold')]
+sns.set(style = "darkgrid")
+plt.tight_layout()
+plt.figure(figsize = (20, 10))
+sns.countplot(x = 'Age', data = gold_medals)
+plt.title('Distribution of Gold Medals vs Age')
+
+# Participation of Women
+women_olympics = final_data[(final_data.Sex == 'F')]
+plt.figure(figsize = (20, 10))
+sns.countplot(x = 'Year', data = women_olympics)
+plt.title('Women participation')
+plt.show()
+
+# Men vs Women over time
+men_dist = final_data[(final_data.Sex == 'M')]
+men = men_dist.groupby('Year')['Sex'].value_counts()
+women_dist = final_data[(final_data.Sex == 'F')]
+women = women_dist.groupby('Year')['Sex'].value_counts()
+plt.figure(figsize = (20, 10))
+men.loc[:,'M'].plot()
+women.loc[:,'F'].plot()
+plt.legend(['Male', 'Female'], loc='upper left')
+plt.title('Male and Female participation over the years ')
+plt.show()
+
+# Indian Medals over the year
+indian_medals = final_data[final_data.Team == 'India']
+plt.figure(figsize = (20, 10))
+plt.tight_layout()
+sns.countplot(x = 'Year', hue = 'Medal', data = indian_medals)
+plt.title("India's Total Medal count")
+plt.show()
 
 # Stratified Sampling - testing/training #214510 	#150154		#64356		
 training_set = final_data[final_data['Year'] < 2000]
