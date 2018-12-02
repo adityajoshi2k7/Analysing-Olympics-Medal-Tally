157313ed0376b27f3a1a5a6a7bbb0ba08cdb955e
Arpita Shekhar <ashekha@ncsu.edu>
2018-12-02 02:04:13 -0500

    lstm input shape

--- lstm.py
+++ lstm.py
@@ -20,33 +20,20 @@ def lstm_classifier(final_data):
 	#final_X = final_X.reshape(country_count, year_count, final_X.shape[1])
 	final_X = final_data.groupby("NOC", as_index=True)['Year', 'Sex', 'Age', 'Height', 'Weight', 'Host_Country', 'Sport'].apply(lambda x: x.values.tolist())
 	final_Y = final_data.groupby("NOC", as_index=True)['Medal'].apply(lambda x: x.values.tolist())
-	print(len(final_X), ' ', len(final_X[0]), ' ', len(final_X[0][0]))
+	final_X = pad_sequences(final_X, maxlen=None, dtype='float', padding='post', truncating='post', value=0.0)
 
-	print('\n', len(final_X), ' ', len(final_X[1]), ' ', len(final_X[1][0]))
-
-	print('\n', len(final_X), ' ', len(final_X[2]), ' ', len(final_X[2][0]))
-
-	final_X = pad_sequences(final_X, maxlen=None, dtype='int32', padding='post', truncating='post', value=0.0)
-
-	print('\nAfter:\n', len(final_X), ' ', len(final_X[0]), ' ', len(final_X[0][0]))
-
-	print('\n', len(final_X), ' ', len(final_X[1]), ' ', len(final_X[1][0]))
-
-	print('\n', len(final_X), ' ', len(final_X[2]), ' ', len(final_X[2][0]))
-	# X_train = np.hstack(final_X).reshape(len(final_X), 7, 7)
-	# y_train = np.hstack(final_Y).reshape(len(final_X), 7)
-
-	# print(X_train.shape)
+	print(final_X.shape)
 
 	# define model - 10 hidden nodes
 	model = Sequential()
-	model.add(LSTM(10, input_shape = (len(final_X), len(final_X[0][0])), return_sequences = True, batch_size = 1000))
-	model.add(Dense(4, activation = 'sigmoid'))
+	model.add(LSTM(10, batch_input_shape = (2, len(final_X[0]), len(final_X[0][0])), return_sequences = True, stateful=True))
+	model.add( LSTM(10, return_sequences = False))
+	model.add(Dense(1, activation = 'sigmoid'))
 	model.summary()
 	model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 
 	# fit network
-	history = model.fit(final_X, final_Y, epochs = 10, batch_size = 50)
+	history = model.fit(final_X, final_Y, epochs = 10, batch_size = 2, verbose = 2)
 
 	loss, accuracy = model.evaluate(final_X, final_Y)
 	print(accuracy)
